8340ef6cf5d399126dfa0cd924cb262319dd5a23
Faiz Kazi <faiz@titan.ocn.ne.jp>
2024-09-06 17:37:55 +0900

    DONE: p2 (lastBut1)

--- haskell/src/P99/Lists.hs
+++ haskell/src/P99/Lists.hs
@@ -1,8 +1,14 @@
 module P99.Lists
     ( myLast
+    , lastBut1
     ) where
 
 myLast :: [a] -> Maybe a
 myLast [a] = Just a
 myLast [] = Nothing
 myLast (_:rest) = myLast rest
+
+lastBut1 :: [a] -> Maybe a
+lastBut1 [a, _] = Just a
+lastBut1 [] = Nothing
+lastBut1 (_:rest) = lastBut1 rest
--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -2,6 +2,7 @@
 
 import Test.Hspec
 import Test.QuickCheck
+import Test.Hspec.QuickCheck
 
 import Data.Maybe (isNothing)
 
@@ -16,3 +17,12 @@ main = hspec $ do
         l@[] -> isNothing (P99.myLast l)
         l    -> P99.myLast l == Just (last l :: Int)
 
+  describe "P02 (*) Find the last but one element of a list. " $ do
+    describe "lastBut1" $ do
+      prop "returns the second-last element of a big enough list" $
+        \l -> (length l >= 2) ==> P99.lastBut1 (l:: [Int])
+              `shouldBe` Just (head $ tail $ reverse l)
+      prop "should return Nothing for a singleton list" $
+        \a -> P99.lastBut1 [a::Int] `shouldBe` Nothing
+      it "should return Nothing for an empty list" $ do
+        P99.lastBut1 ([]::[Int]) `shouldBe` Nothing
